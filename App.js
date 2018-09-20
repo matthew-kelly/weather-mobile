3da80651e55d632b6710fef3c2d6ec1fb679b35a
Matt Kelly <bmatthewkelly@gmail.com>
2018-09-20 14:36:00 -0700

    add Dark Sky API request

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { DARK_SKY_API_KEY } from 'react-native-dotenv';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      lat: 49.3327862,
+      long: -123.1008816
+    }
+  }
+
+  componentDidMount() {
+    this.getWeather();
+  }
+
+  getWeather = () => {
+    return fetch(`https://api.darksky.net/forecast/${DARK_SKY_API_KEY}/${this.state.lat},${this.state.long}`)
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({
+          isLoading: false,
+          weatherInfo: responseJson
+        })
+      })
+      .catch(e => console.error(e));
+  }
+
   render() {
+    let weatherData;
+    if (this.state.isLoading) {
+      weatherData = <Text>Loading</Text>
+    } else {
+      weatherData = <Text>{this.state.weatherInfo.currently.summary}</Text>
+    }
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        {weatherData}
+        <Text>Powered by Dark Sky</Text>
       </View>
     );
   }
